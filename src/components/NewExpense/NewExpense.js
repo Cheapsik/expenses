c3cdfdfa1eb5c979dfbe582f1becf9b9b4a860f5
Cheapik <cheapsik@gmail.com>
2021-06-03 21:55:59 +0200

    feat: Added Toastify library

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,38 @@
 import React, { useState } from "react";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
+
+toast.configure({
+  autoClose: 2000,
+  draggable: true,
+});
 
 const NewExpense = (props) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const saveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = {
-      ...enteredExpenseData,
-      id: Math.random().toString(),
-    };
-    props.onAddExpense(expenseData);
-    setIsEditing(false);
+    if (
+      enteredExpenseData.title.trim().length === 0 ||
+      enteredExpenseData.amount === 0
+    ) {
+      toast.error("Enter correct data! 😔", {
+        position: "bottom-right",
+        pauseOnHover: false,
+      });
+    } else {
+      const expenseData = {
+        ...enteredExpenseData,
+        id: Math.random().toString(),
+      };
+      props.onAddExpense(expenseData);
+      setIsEditing(false);
+      toast.success("Added a expense! 🤗", {
+        position: "bottom-right",
+        pauseOnHover: false,
+      });
+    }
   };
 
   const startEditingHandler = () => {
